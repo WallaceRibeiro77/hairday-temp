475221a59d7a9bbf25de2cbe0e43aba1d49eaa01
wallace ribeiro <wallaceribeiro72@gmail.com>
2025-04-01 16:38:17 -0300

    create utils, libs and modules folders

---
+++ src/modules/form/submit.js
@@ -0,0 +1,17 @@
+import dayjs from "dayjs"
+
+const form = document.querySelector("form")
+const selectedDate = document.querySelector("#date")
+
+// Data atual para formatar o input
+const inputToday = dayjs(new Date()).format("YYYY-MM-DD")
+
+//carrega a data atual e define a data minima como sendo a data atual.
+selectedDate.value = inputToday
+selectedDate.min = inputToday
+
+form.onsubmit = (e) => {
+    e.preventDefault()
+
+
+}
